2219c82ef8e109c314138d9334e09d2ea68dee67
michelleresian <michellresian@gmail.com>
2023-10-04 00:11:26 +0300

    python task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,26 +1,26 @@
-#include "lists.h"
+# include "lists.h"
 /**
- * check_cycle - checks if a linked list has a recurring cyc
+* check_cycle - checks if a linked list has a recurring cyc
 le
- * list: a pointer to the linked list in question
- * Return: 1 if a cycle is foun and 0 if a failure is not fo
+* list: a pointer to the linked list in question
+* Return: 1 if a cycle is foun and 0 if a failure is not fo
 und
- */
-int check_cycle(listint_t *list)
+*/
+int check_cycle(listint_t * list)
 {
-    listint_t *current;
-    listint_t *power;
+    listint_t * current
+    listint_t * power
 
-    current = list;
-    power = list;
+    current = list
+    power = list
 
-    while (current != NULL && power != NULL && power->next)
+    while (current != NULL & & power != NULL & & power -> next)
     {
-        current = current->next;
-        power = power->next->next;
+        current = current -> next
+        power = power -> next -> next
 
         if (current == power)
-            return (1);
+        return (1)
     }
-    return (0);
+    return (0)
 }
