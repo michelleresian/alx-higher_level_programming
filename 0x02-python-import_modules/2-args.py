340c1003adefc11023823eba320cd4c72875c3e0
michelleresian <michellresian@gmail.com>
2023-10-11 01:01:37 +0300

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
+import sys
 
-    argsl = len(sys.argv) - 1
-    if argsl == 0:
-        print("0 arguments.")
-    elif argsl == 1:
-        print("1 argument:")
+
+def main():
+    args = sys.argv[1:]  # Exclude the script name from the arguments
+    num_args = len(args)
+
+    print(f"{num_args}", end=' ')
+    if num_args == 0:
+        print(".", end='\n\n')
+    elif num_args == 1:
+        print("argument:", end='\n')
+        print(f"{num_args}:", args[0])
     else:
-        print("{} arguments:".format(argsl))
-    for i in range(argsl):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("arguments:", end='\n')
+        for i, arg in enumerate(args, start=1):
+            if i == 0:
+                continue
+            print(f"{i}: {arg}")
+
+
+if __name__ == '__main__':
+    main()
+    
