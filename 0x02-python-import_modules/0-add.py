f13f4e5f62d4cd69b33a53c756bd57ab07e51cc5
michelleresian <michellresian@gmail.com>
2023-10-11 00:47:24 +0300

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
+from add_0 import add
+
+
+def main():
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+
+    sum = add(a, b)
+
+    print("{:d} + {:d} = {:d}".format(a, b, sum))
+    
+
+    if __name__ == '__main__':
+        main()
