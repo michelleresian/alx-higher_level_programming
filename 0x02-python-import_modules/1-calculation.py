f8c1ae54da48068c8c5985e20ff35cfab4863a5f
michelleresian <michellresian@gmail.com>
2023-10-11 05:57:36 +0300

    task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-def add(a, b):
-    a = 10
-    b = 5
+if __name__ == '__main__':
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    from calculator_1 import add, sub, mul, div
+    array = [add, sub, mul, div]
+    operator = ["+", "-", "*", "/"]
+
+a = 10
+b = 5
+for i in range(0, len(array)):
+    print("{0} {1} {2} = {3}".format(a, operator[i], b, array[i](a, b)))
