b768f5808e71dc3ae8ef902d7c902151076cd26f
michelleresian <michellresian@gmail.com>
2023-10-23 22:21:24 +0300

    task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -12,5 +12,5 @@ def safe_print_list_integers(my_list=[], x=0):
         except TypeError:
             pass
 
-        print()
-        return printed_intergers
+    print()
+    return printed_intergers
