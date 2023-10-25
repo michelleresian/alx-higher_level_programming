fb4fdd38d499511267c6b60543a726a81a72eb5d
michelleresian <michellresian@gmail.com>
2023-10-25 04:59:44 +0300

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """Define square class"""
+
+    def __init__(self, size=0):
+        """Initialize class"""
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.size = size
+
+    @property
+    def size(self):
+        """int: private size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets value into size, must be int"""
+
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        """Area of a square"""
+
+        return self.__size**2
+
+    def my_print(self):
+        """Prints the square with the character '#' to stdout."""
+
+        if self.__size != 0:
+            square = '#' * self.__size + '\n'
+            print(square * self.__size)
+        else:
+            print()
